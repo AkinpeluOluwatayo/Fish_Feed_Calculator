50fd231bb4360b7133293f64e865d7cf5fc52070
AkinpeluOluwatayo <akinpeluoluwatayo1235@gmail.com>
2025-09-24 10:57:08 +0100

    updated

---
+++ src/FishInfo.java
@@ -0,0 +1,14 @@
+public class FishInfo {
+    private String species;
+    private String age;
+    private String weight;
+    private String count;
+
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public String getSpecies() {
+        return species;
+    }
+}
